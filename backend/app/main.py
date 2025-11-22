8defb7b8ce5173f4621cc95907fb38014674bc80
Ryley Mao <rymao_@outlook.com>
2025-11-21 22:38:26 -0500

    Set up Backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-app.add
+@app.get("/")
+def root():
+    return {"message": "Whatscookin AI"}
 
-
-
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000)
